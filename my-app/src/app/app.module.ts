e0b04ccba33bfd2779882bb4425125f0dcb9299c
Omri Mizrahi <mazmaz2k@gmail.com>
2017-03-07 11:30:14 +0200

    texts

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,10 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { ItemComponent } from './item/item.component';
+import { LitemComponent } from './litem/litem.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    ItemComponent,
+    LitemComponent,
+    LitemComponent
   ],
   imports: [
     BrowserModule,
---
+++ my-app/src/app/list/list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+myList =[
+  {
+    id: 1 
+  },
+    {
+      id:  2
+    },
+    {
+      id: 3
+    },
+      {
+        id:4
+      }
+      ];
+}
