0912983dc375d89d84e670e9957243209555eeb3
Omri Mizrahi <mazmaz2k@gmail.com>
2017-03-08 16:08:23 +0200

    curraction

--- ex1/my-app/src/app/app.module.ts
+++ ex1/my-app/src/app/app.module.ts
@@ -4,17 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ListComponent } from './list/list.component';
-import { ItemComponent } from './item/item.component';
-import { LitemComponent } from './litem/litem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent,
-    ItemComponent,
-    LitemComponent,
-    LitemComponent
+
   ],
   imports: [
     BrowserModule,
